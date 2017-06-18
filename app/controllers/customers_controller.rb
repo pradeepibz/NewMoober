c18d03968febde6f50b89eb39c28a41c313467a9
Pradeepibz <pradeep.loganathan@itbeezone.in>
2017-06-18 17:52:43 +0530

    Subscriber create

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,4 +7,17 @@ class CustomersController < ApplicationController
 
   def forget_password
   end
+
+  def create_subscribe
+		@subscribe = Subscriber.new(subscribe_params)
+		if @subscribe.save
+			CustomerMailer.subscribe_mail(@subscribe).deliver
+		end
+  end
+
+  private
+
+  def subscribe_params
+		params.require(:subscribe).permit(:email)
+  end
 end
