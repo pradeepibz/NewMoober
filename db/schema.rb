e7bf0ef98eb0fdae06002df5094b014290cf2fdd
Karthick Nagarajan <karthickibz@gmail.com>
2017-06-19 11:04:27 +0530

    solve footer issue

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20170618132111) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
