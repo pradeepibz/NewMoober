df902cc5047c5ffd0dca49ec12ac505def428e70
Karthick Nagarajan <karthickibz@gmail.com>
2017-06-22 10:55:40 +0530

    disable double tab to zoom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,23 @@
 //= require iscroll
 
 
+(function($) {
+	$.fn.nodoubletapzoom = function() {
+		$(this).bind('touchstart', function preventZoom(e) {
+			var t2 = e.timeStamp
+			, t1 = $(this).data('lastTouch') || t2
+			, dt = t2 - t1
+			, fingers = e.originalEvent.touches.length;
+			$(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+	};
+})(jQuery);
+
 $(document).ready(function(){
 
 	// Finding Desktop or Mobile and hide appropriate navigation
