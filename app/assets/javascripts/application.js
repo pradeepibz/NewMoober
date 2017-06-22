d6b05c688c1e5e6b3cb68d9acd501322d97fe552
Karthick Nagarajan <karthickibz@gmail.com>
2017-06-22 11:08:05 +0530

    Double tab to zoom Partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,23 +24,43 @@
 //= require mobile_nav
 //= require iscroll
 
+// disable double tab to zoom 
 
-(function($) {
-	$.fn.nodoubletapzoom = function() {
-		$(this).bind('touchstart', function preventZoom(e) {
-			var t2 = e.timeStamp
-			, t1 = $(this).data('lastTouch') || t2
-			, dt = t2 - t1
-			, fingers = e.originalEvent.touches.length;
-			$(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+// (function($){
+//     // Determine if we on iPhone or iPad
+//     var isiOS = false;
+//     var agent = navigator.userAgent.toLowerCase();
+//     if(agent.indexOf('iphone') >= 0 || agent.indexOf('ipad') >= 0){
+//     	isiOS = true;
+//     }
 
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-	};
-})(jQuery);
+//     $.fn.doubletap = function(onDoubleTapCallback, onTapCallback, delay){
+//     	var eventName, action;
+//     	delay = delay == null? 500 : delay;
+//     	eventName = isiOS == true? 'touchend' : 'click';
+
+//     	$(this).bind(eventName, function(event){
+//     		var now = new Date().getTime();
+//     		var lastTouch = $(this).data('lastTouch') || now + 1 * the first time this will make delta a negative number ;
+//     		var delta = now - lastTouch;
+//     		clearTimeout(action);
+//     		if(delta<500 && delta>0){
+//     			if(onDoubleTapCallback != null && typeof onDoubleTapCallback == 'function'){
+//     				onDoubleTapCallback(event);
+//     			}
+//     		}else{
+//     			$(this).data('lastTouch', now);
+//     			action = setTimeout(function(evt){
+//     				if(onTapCallback != null && typeof onTapCallback == 'function'){
+//     					onTapCallback(evt);
+//     				}
+//                     clearTimeout(action);   // clear the timeout
+//                   }, delay, [event]);
+//     		}
+//     		$(this).data('lastTouch', now);
+//     	});
+//     };
+// })(jQuery);
 
 $(document).ready(function(){
 
