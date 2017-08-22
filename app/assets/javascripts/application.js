78de96bdd6da201d6afc68ed589cbbcb64b27a85
Gopinathibz <gopinath.ibz@gmail.com>
2017-08-22 12:03:41 +0530

    image missing issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -100,12 +100,12 @@ $(document).on('click', '.toggle-is-active', function(){
 
 function validateQty(event) {
     var key = window.event ? event.keyCode : event.which;
-if (event.keyCode == 8 || event.keyCode == 46
- || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 9) {
-    return true;
-}
-else if ( key < 48 || key > 57 ) {
-    return false;
-}
-else return true;
+  if (event.keyCode == 8 || event.keyCode == 46
+   || event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 9) {
+      return true;
+  }
+  else if ( key < 48 || key > 57 ) {
+      return false;
+  }
+  else return true;
 };
