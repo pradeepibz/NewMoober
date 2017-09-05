fb3adfe04f6b33d3412eae936f7f0530ba61c711
Gopinathibz <gopinath.ibz@gmail.com>
2017-09-05 19:06:39 +0530

    home page issue

--- app/assets/javascripts/channels/proposal_list.js
+++ app/assets/javascripts/channels/proposal_list.js
@@ -10,52 +10,52 @@ $(document).ready(function(){
       console.log('disconnected auction');
     },
     received: function(data) {
-      var current_user = localStorage.getItem('user_id');
-      var API_URL = "http://45.56.72.52/api/userapi/";
-      var proposal_params = '{"user_id": '+ current_user +'}'
-      if (current_user != "" && current_user != null && current_user != undefined){
-        $.ajax({
-          url: API_URL+"getMovingRequestList",
-          type: 'POST',
-          data: proposal_params,
-        })
-        .done(function(data) {
-          var count = data.data.upcoming.length
-          console.log(count)
-          localStorage.setItem('moves_count', count);
-          $('.moves-count mark').text(count);
-          $('.side-menu-moves-count mark').text(count);
-          $('.upcoming_values').html("")
-          $('.past_values').html("")
-          $.each(data.data.upcoming ,function(key, res){
-            var upcoming = '<div class="col-md-12 upcome-border"><div class="col-md-8 um-values"><div class="moving-requests" data-request-id="'+res.moving_request_id+'" data-from-add="'+res.from_address+'">'+redNotification(res)+'</div><p>Awaiting Proposals</p><p>'+res.room_type_name+'</p><p>'+res.move_date_time+'</p><p>'+res.from_address+'</p><p>'+res.to_address+''+yellowNotification(res)+'</p></div><div class="col-md-4 upcoming-images_'+res.moving_request_id+'"></div></div>'
-            $('.upcoming_values').append(upcoming)
-            if (res.moving_request_images.length > 0){ 
-              $.each(res.moving_request_images, function(i, img){
-                var images = '<div class="col-md-6"><img src="'+img.image_thumb+'"></div>'
-                $('.upcoming-images_'+res.moving_request_id+'').append(images)
-              })
-            }else{
-              var images = '<div class="col-md-6">No Images</div>'
-              $('.upcoming-images_'+res.moving_request_id+'').append(images)
-            }
-          });
+      // var current_user = localStorage.getItem('user_id');
+      // var API_URL = "http://45.56.72.52/api/userapi/";
+      // var proposal_params = '{"user_id": '+ current_user +'}'
+      // if (current_user != "" && current_user != null && current_user != undefined){
+      //   $.ajax({
+      //     url: API_URL+"getMovingRequestList",
+      //     type: 'POST',
+      //     data: proposal_params,
+      //   })
+      //   .done(function(data) {
+      //     var count = data.data.upcoming.length
+      //     console.log(count)
+      //     localStorage.setItem('moves_count', count);
+      //     $('.moves-count mark').text(count);
+      //     $('.side-menu-moves-count mark').text(count);
+      //     $('.upcoming_values').html("")
+      //     $('.past_values').html("")
+      //     $.each(data.data.upcoming ,function(key, res){
+      //       var upcoming = '<div class="col-md-12 upcome-border"><div class="col-md-8 um-values"><div class="moving-requests" data-request-id="'+res.moving_request_id+'" data-from-add="'+res.from_address+'">'+redNotification(res)+'</div><p>Awaiting Proposals</p><p>'+res.room_type_name+'</p><p>'+res.move_date_time+'</p><p>'+res.from_address+'</p><p>'+res.to_address+''+yellowNotification(res)+'</p></div><div class="col-md-4 upcoming-images_'+res.moving_request_id+'"></div></div>'
+      //       $('.upcoming_values').append(upcoming)
+      //       if (res.moving_request_images.length > 0){ 
+      //         $.each(res.moving_request_images, function(i, img){
+      //           var images = '<div class="col-md-6"><img src="'+img.image_thumb+'"></div>'
+      //           $('.upcoming-images_'+res.moving_request_id+'').append(images)
+      //         })
+      //       }else{
+      //         var images = '<div class="col-md-6">No Images</div>'
+      //         $('.upcoming-images_'+res.moving_request_id+'').append(images)
+      //       }
+      //     });
 
-          $.each(data.data.past ,function(key, res){
-            var upcoming = '<div class="col-md-12 upcome-border"><div class="col-md-8 um-values"><div class="moving-requests" data-request-id="'+res.moving_request_id+'" data-from-add="'+res.from_address+'">'+redNotification(res)+'</div><p>Awaiting Proposals</p><p>'+res.room_type_name+'</p><p>'+res.move_date_time+'</p><p>'+res.from_address+'</p><p>'+res.to_address+''+yellowNotification(res)+'</p></div><div class="col-md-4 past-images_'+res.moving_request_id+'"></div></div>'
-            $('.past_values').append(upcoming)
-            if (res.moving_request_images.length > 0){ 
-              $.each(res.moving_request_images, function(i, img){
-                var images = '<div class="col-md-6"><img src="'+img.image_thumb+'"></div>'
-                $('.past-images_'+res.moving_request_id+'').append(images)
-              })
-            }else{
-              var images = '<div class="col-md-6">No Images</div>'
-              $('.past-images_'+res.moving_request_id+'').append(images)
-            }
-          });
-        });
-      }
+      //     $.each(data.data.past ,function(key, res){
+      //       var upcoming = '<div class="col-md-12 upcome-border"><div class="col-md-8 um-values"><div class="moving-requests" data-request-id="'+res.moving_request_id+'" data-from-add="'+res.from_address+'">'+redNotification(res)+'</div><p>Awaiting Proposals</p><p>'+res.room_type_name+'</p><p>'+res.move_date_time+'</p><p>'+res.from_address+'</p><p>'+res.to_address+''+yellowNotification(res)+'</p></div><div class="col-md-4 past-images_'+res.moving_request_id+'"></div></div>'
+      //       $('.past_values').append(upcoming)
+      //       if (res.moving_request_images.length > 0){ 
+      //         $.each(res.moving_request_images, function(i, img){
+      //           var images = '<div class="col-md-6"><img src="'+img.image_thumb+'"></div>'
+      //           $('.past-images_'+res.moving_request_id+'').append(images)
+      //         })
+      //       }else{
+      //         var images = '<div class="col-md-6">No Images</div>'
+      //         $('.past-images_'+res.moving_request_id+'').append(images)
+      //       }
+      //     });
+      //   });
+      // }
     },
     redNotification: function(req){
       if (res.notification_count > 0){
